305b4fe07107776893f273eea325dea94c14384c
Joohyoung Jun <joohyoung@Joohyoungui-MacBookAir.local>
2026-06-24 14:47:39 +0900

    feat: jwt guard 추가 및 모듈 등록

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,10 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '@/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtRefreshStrategy } from './jwt/jwt-refresh.strategy';
+import { JwtAccessStrategy } from './jwt/jwt-access.strategy';
 
 @Module({
-  imports: [UsersModule, JwtModule.register({})],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule, JwtModule.register({})],
+  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
