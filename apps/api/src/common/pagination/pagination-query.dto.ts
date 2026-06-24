a8a85ad19c80b2ba93424c6da2d40399fa3942dc
Joohyoung Jun <joohyoung@Joohyoungui-MacBookAir.local>
2026-06-24 23:41:18 +0900

    pagination dto 추가

---
+++ apps/api/src/common/pagination/pagination-query.dto.ts
@@ -0,0 +1,26 @@
+// common/pagination/pagination-query.dto.ts
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class PaginationQueryDto {
+  @ApiPropertyOptional({ description: '페이지 번호', default: 1, minimum: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+
+  @ApiPropertyOptional({
+    description: '페이지당 개수',
+    default: 10,
+    minimum: 1,
+    maximum: 100,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  pageSize: number = 10;
+}
