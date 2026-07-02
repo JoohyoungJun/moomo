d727004527ff5e437692cb82298052f64d5da33a
Joohyoung Jun <joohyoung@Joohyoungui-MacBookAir.local>
2026-07-02 23:30:05 +0900

    chore: 이름 변경 및 Suspense 적용

--- apps/web/app/(with-header)/posts/page.tsx
+++ apps/web/app/(with-header)/posts/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import PostListPage from '@/components/posts/post-list/PostListPage';
 
 export default function Page() {
-  return <PostListPage />;
+  return (
+    <Suspense fallback={<p>게시글을 불러오는 중...</p>}>
+      <PostListPage />
+    </Suspense>
+  );
 }
