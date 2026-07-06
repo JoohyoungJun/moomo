0bee2e1ccecf3caba67e437bd5b3ba27e3288bab
Joohyoung Jun <joohyoung@Joohyoungui-MacBookAir.local>
2026-07-06 22:51:21 +0900

    fix: 상품 리스트 페이지 suspense 적용

--- apps/web/app/(with-header)/products/page.tsx
+++ apps/web/app/(with-header)/products/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import ProductListPage from '@/components/products/product-list/ProductListPage';
 
 export default function Page() {
-  return <ProductListPage />;
+  return (
+    <Suspense fallback={<p>상품을 불러오는 중...</p>}>
+      <ProductListPage />
+    </Suspense>
+  );
 }
